5fc558cdcd711e5fff123961b8cd7d3a356ecec6
Mitchelde <tretyakovanton0000@gmail.com>
2023-11-13 11:44:03 +0300

    add connect to table Employees and Companies

--- main.py
+++ main.py
@@ -1,95 +1,240 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+import pyodbc
+import telebot
 
-# Define conversation states
-CHOOSING, COMPANY, EMPLOYEE = range(3)
+# Устанавливаем соединение с базой данных
+connectionString = ('DRIVER={SQL Server};'
+                    'SERVER=DESKTOP-PP1PFRF\SQLEXPRESS;'
+                    'DATABASE=TalentConnectDB;'
+                    'Trusted_Connection=yes;'  # Использовать Windows аутентификацию
+                    )
 
-# Global variables to store user input
-user_data = {}
+# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
+TOKEN = '6318182674:AAEEnk6kg86nnwcCpX3VZPz_K2G-iy_ms5M'
+nameCompany = ""
+nameEmployee = ""
+contactPersonName = ""
+jobTitle = ""
+salaryForCompany = 0
+salaryForEmployee = 0
+contactInfo = ""
+aboutCompany = ""
+aboutEmployee = ""
+prospectiveEmployees = ""
 
-# Function to start the conversation
-def start(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text(
-        "Привет! Этот бот поможет вам зарегистрироваться как компания или работник. "
-        "Используйте /register, чтобы начать."
-    )
-    return CHOOSING
+# Создаем объект бота
+bot = telebot.TeleBot(TOKEN)
 
-# Function to handle the /register command
-def register(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text(
-        "Выберите, кто вы: компания или работник?",
-        reply_markup={
-            "keyboard": [["Компания", "Работник"]],
-            "one_time_keyboard": True,
-            "resize_keyboard": True,
-        },
-    )
-    return CHOOSING
 
-# Function to handle the choice of company or employee
-def choose_type(update: Update, context: CallbackContext) -> int:
-    user_data["type"] = update.message.text
-    update.message.reply_text(f"Вы выбрали: {user_data['type']}. Теперь укажите {user_data['type']}.\n"
-                              "Введите название:")
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Привет! Я твой простой бот.")
 
-    return COMPANY if user_data["type"] == "Компания" else EMPLOYEE
 
-# Function to handle user input during registration
-def process_input(update: Update, context: CallbackContext) -> int:
-    user_input = update.message.text
-    user_data["input"] = user_input
+# Обработчик команды /help
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.chat.id, "Это простой бот. Введите /start для начала.")
 
-    # Ask for additional information based on user type
-    if user_data["type"] == "Компания":
-        prompts = ["Индустрия", "Описание", "Контактное лицо", "Email", "Телефон"]
-    else:  # "Работник"
-        prompts = ["Имя", "Фамилия", "Должность", "Зарплата", "Email", "Телефон"]
 
-    next_prompt(update, prompts)
-    return COMPANY if user_data["type"] == "Компания" else EMPLOYEE
+# Обработчик команды /help
+@bot.message_handler(commands=['dbtest'])
+def handle_help(message):
+    # Создаем объект курсора для выполнения SQL-запросов
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
 
-# Function to handle the end of the registration process
-def end_registration(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text(f"Регистрация {user_data['type']} завершена!\n\n"
-                              f"Ваше название: {user_data['input']}\n"
-                              f"Ваши данные: {user_data}")
+    # Пример выполнения запроса
+    cursor.execute('SELECT * FROM Companies')
 
-    # Here you can save the registration information to your database
-    # For example, user_data["type"], user_data["input"], etc.
+    # Получаем результат запроса
+    for row in cursor:
+        bot.send_message(message.chat.id, f"{row}")
 
-    user_data.clear()  # Clear user_data for the next registration
-    return ConversationHandler.END
+    # Закрываем соединение
+    conn.close()
 
-# Function to prompt the user for the next information
-def next_prompt(update: Update, prompts: list):
-    if prompts:
-        prompt = prompts.pop(0)
-        update.message.reply_text(f"Введите {prompt}:")
-    else:
-        end_registration(update, None)
 
-def main():
-    # Set up the Telegram bot
-    updater = Updater("6318182674:AAEEnk6kg86nnwcCpX3VZPz_K2G-iy_ms5M", use_context=True)
-    dp = updater.dispatcher
+# NOTE: Добавить возможность отменить и изменить введенные данные
+# Обработчик команды /companyregistation
+# region Регистрация компании
+@bot.message_handler(commands=['companyregistation'])
+def handle_help(message):
+    bot.send_message(message.chat.id, "Введите Название компании:")
+    bot.register_next_step_handler(message, name_company)
 
-    # Define the conversation handler
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            CHOOSING: [MessageHandler(Filters.regex(r'^(Компания|Работник)$'), choose_type)],
-            COMPANY: [MessageHandler(Filters.text & ~Filters.command, process_input)],
-            EMPLOYEE: [MessageHandler(Filters.text & ~Filters.command, process_input)],
-        },
-        fallbacks=[MessageHandler(Filters.text & ~Filters.command, process_input)],
-    )
 
-    dp.add_handler(conv_handler)
+def name_company(message):
+    global nameCompany
+    nameCompany = message.text.strip()
+    bot.send_message(message.chat.id, "Введите Имя Контактного лица:")
+    bot.register_next_step_handler(message, contact_person_name)
 
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
 
+def contact_person_name(message):
+    global contactPersonName
+    contactPersonName = message.text.strip()
+    bot.send_message(message.chat.id, "Введите Зарплату:")
+    bot.register_next_step_handler(message, salary_for_employee)
+
+
+def salary_for_employee(message):
+    try:
+        global salaryForEmployee
+        salaryForEmployee = int(message.text.strip())
+
+        # Continue with the next step
+        bot.send_message(message.chat.id, "Введите контактную информацию (телефон, почта и тд):")
+        bot.register_next_step_handler(message, contact_info_company)
+
+    except ValueError:
+        # If the user didn't enter a valid number, handle the error
+        bot.send_message(message.chat.id, "Пожалуйста, введите корректную сумму зарплаты. Это должно быть число.")
+        bot.register_next_step_handler(message, salary_for_employee)
+
+
+def contact_info_company(message):
+    global contactInfo
+    contactInfo = message.text.strip()
+    bot.send_message(message.chat.id, "Введите Информацию о себе:")
+    bot.register_next_step_handler(message, about_company)
+
+
+def about_company(message):
+    global aboutCompany
+    aboutCompany = message.text.strip()
+    bot.send_message(message.chat.id, "Введите профессии необходимые Вам:")
+    bot.register_next_step_handler(message, prospective_employees)
+
+
+def prospective_employees(message):
+    global prospectiveEmployees
+    conn = pyodbc.connect(connectionString)
+    prospectiveEmployees = message.text.strip()
+    bot.send_message(message.chat.id, "Регистрация окончена:\n"
+                                      f"Название компании: {nameCompany}\n"
+                                      f"Имя Контактного лица: {contactPersonName}\n"
+                                      f"Зарплата: {salaryForEmployee}\n"
+                                      f"Контактная информация: {contactInfo}\n"
+                                      f"Информация о компании: {aboutCompany}\n"
+                                      f"Профессии необходимые Вам: {prospectiveEmployees}")
+    # Создаем объект курсора для выполнения SQL-запросов
+    cursor = conn.cursor()
+
+    # Пример выполнения запроса
+    cursor.execute(
+        "INSERT INTO Companies (CompanyName, "
+        "ContactPersonName, "
+        "Salary, "
+        "TelegramUserID, "
+        "ChatID, "
+        "ContactInfo, "
+        "AboutCompany, "
+        "ProspectiveEmployees) "
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        (nameCompany,
+         contactPersonName,
+         salaryForEmployee,
+         message.from_user.id,
+         message.chat.id,
+         contactInfo,
+         aboutCompany,
+         prospectiveEmployees))
+    conn.commit()
+    # Закрываем соединение
+    conn.close()
+
+
+# endregion
+
+# NOTE: Добавить возможность отменить и изменить введенные данные
+# Обработчик команды /employeeregistation
+# region Регистрация сотрудника
+@bot.message_handler(commands=['employeeregistation'])
+def handle_help(message):
+    bot.send_message(message.chat.id, "Введите ФИО:")
+    bot.register_next_step_handler(message, name_employee)
+
+
+def name_employee(message):
+    global nameEmployee
+    nameEmployee = message.text.strip()
+    bot.send_message(message.chat.id, "Введите Зарплату:")
+    bot.register_next_step_handler(message, salary_for_company)
+
+
+def salary_for_company(message):
+    try:
+        global salaryForCompany
+        salaryForCompany = int(message.text.strip())
+
+        # Continue with the next step
+        bot.send_message(message.chat.id, "Введите профессию по которой вы можете работать")
+        bot.register_next_step_handler(message, job_title)
+
+    except ValueError:
+        # If the user didn't enter a valid number, handle the error
+        bot.send_message(message.chat.id, "Пожалуйста, введите корректную сумму зарплаты. Это должно быть число.")
+        bot.register_next_step_handler(message, salary_for_company)
+
+
+def job_title(message):
+    global jobTitle
+    jobTitle = message.text.strip()
+    bot.send_message(message.chat.id, "Введите контактную информацию (телефон, почта и тд)::")
+    bot.register_next_step_handler(message, contact_info_employee)
+
+
+def contact_info_employee(message):
+    global contactInfo
+    contactInfo = message.text.strip()
+    bot.send_message(message.chat.id, "Введите Информацию о себе:")
+    bot.register_next_step_handler(message, about_employee)
+
+
+def about_employee(message):
+    global aboutEmployee
+    aboutEmployee = message.text.strip()
+    conn = pyodbc.connect(connectionString)
+    bot.send_message(message.chat.id, "Регистрация окончена:\n"
+                                      f"ФИО: {nameEmployee}\n"
+                                      f"Зарплата: {salaryForCompany}\n"
+                                      f"Профессия: {jobTitle}\n"
+                                      f"Контактная информация: {contactInfo}\n"
+                                      f"Информация о себе: {aboutEmployee}\n")
+    # Создаем объект курсора для выполнения SQL-запросов
+    cursor = conn.cursor()
+
+    # Пример выполнения запроса
+    cursor.execute(
+        "INSERT INTO Employees ("
+        "EmployeeName, "
+        "DesiredSalary, "
+        "JobTitle, "
+        "TelegramUserID, "
+        "ChatID, "
+        "ContactInfo, "
+        "AboutEmployee)"
+        "VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (nameEmployee,
+         salaryForCompany,
+         jobTitle,
+         message.from_user.id,
+         message.chat.id,
+         contactInfo,
+         aboutEmployee))
+    conn.commit()
+    # Закрываем соединение
+    conn.close()
+# endregion
+
+# Обработчик всех текстовых сообщений
+# @bot.message_handler(func=lambda message: True)
+# def handle_text(message):
+#     text = message.text
+#     bot.send_message(message.chat.id, f'Вы написали: {text}')
+
+
+# Запускаем бота
 if __name__ == "__main__":
-    main()
+    bot.polling(none_stop=True)
