18ccb3c3213e5522fc381e5d822fd3c4bfd9bc0d
Mitchelde <tretyakovanton0000@gmail.com>
2023-11-13 13:46:54 +0300

    add /changecompany and /changeemplayee

--- main.py
+++ main.py
@@ -21,9 +21,79 @@ contactInfo = ""
 aboutCompany = ""
 aboutEmployee = ""
 prospectiveEmployees = ""
-
+isReg = True
 # Создаем объект бота
 bot = telebot.TeleBot(TOKEN)
+
+
+def check_existing_data(chat_id, table_name):
+    # Check if the chat ID already exists in the database
+    conn = pyodbc.connect(connectionString)
+    query = f"SELECT * FROM {table_name} WHERE ChatID = ?"
+    # Создаем объект курсора для выполнения SQL-запросов
+    cursor = conn.cursor()
+    cursor.execute(query, (chat_id,))
+    existing_user = cursor.fetchone()
+    conn.close()
+    return existing_user
+
+
+def update_data_employee(chat_id, new_EmployeeName, new_Salary, new_JobTitle, new_ContactInfo, new_AboutEmployee):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE Employees "
+                   f"SET EmployeeName = ?, "
+                   f"DesiredSalary = ? ,"
+                   f"JobTitle = ? ,"
+                   f"ContactInfo = ? ,"
+                   f"AboutEmployee = ? "
+                   f"WHERE ChatID = ?",
+                   (new_EmployeeName,
+                    new_Salary,
+                    new_JobTitle,
+                    new_ContactInfo,
+                    new_AboutEmployee,
+                    chat_id
+                    ))
+
+    # Commit the changes to the database
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
+
+
+def update_data_company(chat_id,
+                        new_CompanyName,
+                        new_ContactPersonName,
+                        new_Salary,
+                        ContactInfo,
+                        new_AboutCompany,
+                        new_ProspectiveEmployees):
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE Companies "
+                   f"SET CompanyName = ?, "
+                   f"ContactPersonName = ? ,"
+                   f"Salary = ? ,"
+                   f"ContactInfo = ? ,"
+                   f"AboutCompany = ? ,"
+                   f"ProspectiveEmployees = ? "
+                   f"WHERE ChatID = ?",
+                   (new_CompanyName,
+                    new_ContactPersonName,
+                    new_Salary,
+                    ContactInfo,
+                    new_AboutCompany,
+                    new_ProspectiveEmployees,
+                    chat_id
+                    ))
+
+    # Commit the changes to the database
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
 
 
 # Обработчик команды /start
@@ -36,6 +106,24 @@ def handle_start(message):
 @bot.message_handler(commands=['help'])
 def handle_help(message):
     bot.send_message(message.chat.id, "Это простой бот. Введите /start для начала.")
+
+
+# Обработчик команды /changeemplayee
+@bot.message_handler(commands=['changeemplayee'])
+def handle_help(message):
+    global isReg
+    isReg = False
+    bot.send_message(message.chat.id, "Введите ФИО:")
+    bot.register_next_step_handler(message, name_employee)
+
+
+# Обработчик команды /changecompany
+@bot.message_handler(commands=['changecompany'])
+def handle_help(message):
+    global isReg
+    isReg = False
+    bot.send_message(message.chat.id, "Введите Название компании:")
+    bot.register_next_step_handler(message, name_company)
 
 
 # Обработчик команды /dbtestcompanies
@@ -88,8 +176,12 @@ def handle_help(message):
 # region @bot.message_handler(commands=['companyregistation'])
 @bot.message_handler(commands=['companyregistation'])
 def handle_help(message):
-    bot.send_message(message.chat.id, "Введите Название компании:")
-    bot.register_next_step_handler(message, name_company)
+    existing_company = check_existing_data(message.chat.id, 'Companies')
+    if existing_company:
+        bot.send_message(message.chat.id, "You are already registered.")
+    else:
+        bot.send_message(message.chat.id, "Введите Название компании:")
+        bot.register_next_step_handler(message, name_company)
 
 
 def name_company(message):
@@ -148,26 +240,34 @@ def prospective_employees(message):
                                       f"Профессии необходимые Вам: {prospectiveEmployees}")
     # Создаем объект курсора для выполнения SQL-запросов
     cursor = conn.cursor()
-
-    # Пример выполнения запроса
-    cursor.execute(
-        "INSERT INTO Companies (CompanyName, "
-        "ContactPersonName, "
-        "Salary, "
-        "TelegramUserID, "
-        "ChatID, "
-        "ContactInfo, "
-        "AboutCompany, "
-        "ProspectiveEmployees) "
-        "VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-        (nameCompany,
-         contactPersonName,
-         salaryForEmployee,
-         '@'+message.from_user.username,
-         message.chat.id,
-         contactInfo,
-         aboutCompany,
-         prospectiveEmployees))
+    if isReg:
+        # Пример выполнения запроса
+        cursor.execute(
+            "INSERT INTO Companies (CompanyName, "
+            "ContactPersonName, "
+            "Salary, "
+            "TelegramUserID, "
+            "ChatID, "
+            "ContactInfo, "
+            "AboutCompany, "
+            "ProspectiveEmployees) "
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            (nameCompany,
+             contactPersonName,
+             salaryForEmployee,
+             '@' + message.from_user.username,
+             message.chat.id,
+             contactInfo,
+             aboutCompany,
+             prospectiveEmployees))
+    else:
+        update_data_company(message.chat.id,
+                            nameCompany,
+                            contactPersonName,
+                            salaryForEmployee,
+                            contactInfo,
+                            aboutCompany,
+                            prospectiveEmployees)
     conn.commit()
     # Закрываем соединение
     conn.close()
@@ -180,8 +280,12 @@ def prospective_employees(message):
 # region @bot.message_handler(commands=['employeeregistation'])
 @bot.message_handler(commands=['employeeregistation'])
 def handle_help(message):
-    bot.send_message(message.chat.id, "Введите ФИО:")
-    bot.register_next_step_handler(message, name_employee)
+    existing_employee = check_existing_data(message.chat.id, 'Employees')
+    if existing_employee:
+        bot.send_message(message.chat.id, "You are already registered.")
+    else:
+        bot.send_message(message.chat.id, "Введите ФИО:")
+        bot.register_next_step_handler(message, name_employee)
 
 
 def name_employee(message):
@@ -224,6 +328,7 @@ def about_employee(message):
     global aboutEmployee
     aboutEmployee = message.text.strip()
     conn = pyodbc.connect(connectionString)
+    global isReg
     bot.send_message(message.chat.id, "Регистрация окончена:\n"
                                       f"ФИО: {nameEmployee}\n"
                                       f"Зарплата: {salaryForCompany}\n"
@@ -234,23 +339,32 @@ def about_employee(message):
     cursor = conn.cursor()
 
     # Пример выполнения запроса
-    cursor.execute(
-        "INSERT INTO Employees ("
-        "EmployeeName, "
-        "DesiredSalary, "
-        "JobTitle, "
-        "TelegramUserID, "
-        "ChatID, "
-        "ContactInfo, "
-        "AboutEmployee)"
-        "VALUES (?, ?, ?, ?, ?, ?, ?)",
-        (nameEmployee,
-         salaryForCompany,
-         jobTitle,
-         '@'+message.from_user.username,
-         message.chat.id,
-         contactInfo,
-         aboutEmployee))
+    if isReg:
+        cursor.execute(
+            "INSERT INTO Employees ("
+            "EmployeeName, "
+            "DesiredSalary, "
+            "JobTitle, "
+            "TelegramUserID, "
+            "ChatID, "
+            "ContactInfo, "
+            "AboutEmployee)"
+            "VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (nameEmployee,
+             salaryForCompany,
+             jobTitle,
+             '@' + message.from_user.username,
+             message.chat.id,
+             contactInfo,
+             aboutEmployee))
+    else:
+        update_data_employee(message.chat.id,
+                             nameEmployee,
+                             salaryForCompany,
+                             jobTitle,
+                             contactInfo,
+                             aboutEmployee)
+
     conn.commit()
     # Закрываем соединение
     conn.close()
